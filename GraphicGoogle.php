f6879ebcfb42fe9897ee24574a29b5707c63543f
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2022-04-01 18:43:47 +0100

    Update GraphicGoogle.php

--- GraphicGoogle.php
+++ GraphicGoogle.php
@@ -14,7 +14,12 @@ namespace classes\graphic;
     ---*/
 /*Methods:
     __construct() - is the class constroctor
-   
+   + getDataJson($url,$key,$value) - providing a url to a json webservice, a value for the tag ($key) and a $value for the values, the data 
+                                     will be available to be used in a chart
+   + setOptions($options) - reads chart options in accordance with google charts. $option is a string with options like this
+                            example "{ title: 'My activities', is3D: true,}"
+   + includes() - is a mandatory inclusion for google graphics javascript
+   + piechart() - draws a pie chart with the given data and as per the given options
  */
 
 
@@ -26,11 +31,12 @@ class GraphicGoogle{
     public $gData;
     public $options;
 
-        public function __construct(){
-      
+    public function __construct(){  
     }
 
     public function getDataJson($url,$key,$value){
+      //providing a url to a json webservice, a value for the tag ($key) and a $value for the values, the data will be available to be used in a chart
+
       // Takes raw data from the request
       $json = file_get_contents($url);
       // Converts it into a PHP object
@@ -46,30 +52,21 @@ class GraphicGoogle{
       //echo $this->gData;
     }
 
-    /*public function setData($gData){
-      print_r($gData);
-      $this->gData="[";
-      foreach ($gData as $key => $value){
-        $this->gData.="['" . $key . "'," . $value . "],";
-      }
-      $this->gData.="]";
-
-      echo $this->gData;
-    }
-*/
 
     public function setOptions($options){
-
+      //reads chart options in accordance with google charts. $option is a string with options like this example "{ title: 'My activities', is3D: true,}"
       $this->options=$options;
 
     }
 
 
     public function includes(){
+      //is a mandatory inclusion for google graphics javascript
       return ' <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>';
     }
  
     public function piechart(){
+      //draws a pie chart with the given data and as per the given options
       ?>
         <script type="text/javascript">
           google.charts.load('current', {'packages':['corechart']});
