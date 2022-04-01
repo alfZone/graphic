591748cbf1c84f98b7f07ecd87e69277783ee86a
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2022-04-01 18:47:12 +0100

    Update testGraphics.php

--- testGraphics.php
+++ testGraphics.php
@@ -6,23 +6,25 @@
 
 use classes\graphic\GraphicGoogle;
 
-
+//Create the class
 $gr = new GraphicGoogle();
 
+//send the options for the graphic - see google graphics for more details
 $opt="{
   title: 'As minhas atividades',
   is3D: true,
 }";
-
-
-$gr->getDataJson("https://galeria.esmonserrate.org/public//stats/criadores","nome","num");
 $gr->setOptions($opt);
 
+//send the data using json web service
+$gr->getDataJson("https://galeria.esmonserrate.org/public//stats/criadores","nome","num");
+//$gr->setData($dataForGrp);
+
+//HTML with 2 call for tha classe
 ?>
 <html>
   <head>
     <?php echo $gr->includes(); ?>   
-
   </head>
   <body>
     <?php echo $gr->piechart(); ?> 
